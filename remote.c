46d014decf61335be5cc907e3fca72670a79e446
Pierre Colin <48397990+Pierre-Colin@users.noreply.github.com>
2023-11-17 20:09:41 +0100

    Apply GPL licensing recommendations better
    	This commit renames the license file `COPYING` and adds
    	copyright notices in all C files. This follows the
    	recommendations of the GNU project laid out here:
    	https://www.gnu.org/licenses/gpl-howto.html

--- remote.c
+++ remote.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023 Pierre Colin
+ * This file is part of the serve utility.
+ * 
+ * The serve utility is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * The serve utility is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the serve utility.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <arpa/inet.h>
 #include <assert.h>
 #include <errno.h>
